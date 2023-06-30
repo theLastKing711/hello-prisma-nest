5807c6e5cb909f0d9334ba12fd5601ec87a1b079
theLastKing711 <lastking711@protonmail.com>
2023-06-30 03:46:32 +0300

    .

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
-import * as bodyParser from 'body-parser'
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
